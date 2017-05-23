005a17a05e9a8d493bd67da91726e48a1de52311
stepan <info@videnie.biz>
2017-05-23 16:42:07 +0300

    75474

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($) {
+    $('.input_phone').mask("+7(999) 999-9999");
+
+    $('.formstom__slider').owlCarousel({
+        nav: false,
+        loop: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            992: {
+                items: 2,
+                margin: 20
+            },
+            1024: {
+                items: 3,
+                margin: 40
+            }
+        }
+    });
+
+
+});
